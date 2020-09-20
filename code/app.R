de02ca5b0a166ec486acc6756169c634a862b21c
dblander <dblander@umass.edu>
2020-09-20 14:56:45 -0400

    WIP on Shiny app
    Still working on the shiny app. Currently struggling with the "csv" file names. There are many periods in the file name other than the last ".csv". This is what may need to be changed in order to have the shiny app be successful. Any help with this would be lovely

---
+++ code/app.R
@@ -0,0 +1,16 @@
+#This is the Shiny app for the Neon data
+#Author: Dvir Blander
+#First loading in the shiny library
+library(shiny)
+
+#Note: The files are loaded onto the local machine. The folder should be on GitHub and it's name is NeonFiles.
+#Make sure to set the working directory as the GitHub "code" folder.
+#Looping through the folder to load each csv file separately.
+#Still WIP
+neonFiles <- list.files(path = "./NeonFiles/", pattern = "*.csv")
+#myfiles <- lapply(neonFiles, read.delim)
+
+#bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+ui <- fluidPage()
+server <- function(input, output) {}
+shinyApp(ui =ui, server = server)
