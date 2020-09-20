b5ab2fda0db2ce3aa3cd1519757325d02e6ac84f
dblander <dblander@umass.edu>
2020-09-20 18:08:19 -0400

    Fixed the directory issue
    Started making a little progress with the shiny app. Have a title ready to go. Next step is to have the option to choose the 40 files and then play with each one.

--- code/app.R
+++ code/app.R
@@ -1,16 +1,23 @@
 #This is the Shiny app for the Neon data
 #Author: Dvir Blander
-#First loading in the shiny library
+#First loading in the shiny, dplyr, readr libraries.
+#The shiny library is used because this is a shiny app.
+#The dplyr and readr libraries are used to help read in the data. 
 library(shiny)
+library(dplyr)
+library(readr)
 
 #Note: The files are loaded onto the local machine. The folder should be on GitHub and it's name is NeonFiles.
-#Make sure to set the working directory as the GitHub "code" folder.
-#Looping through the folder to load each csv file separately.
-#Still WIP
-neonFiles <- list.files(path = "./NeonFiles/", pattern = "*.csv")
-myfiles <- lapply(neonFiles, read.delim)
+#Make sure to set the working directory as the GitHub "NeonFiles" folder.
+#This can be done by clicking Session --> Set Working Directory --> Choose Directory. Then navigate to this directory.
+#Loading in the csv files
+neonFiles <- list.files(pattern = "*.csv")
+dat<-lapply(neonFiles,read.csv)
 
-#bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
-ui <- fluidPage()
+#Next step
+#Now need to add option to choose which file to look at.
+ui <- fluidPage(
+  titlePanel("NEON Shiny App")
+)
 server <- function(input, output) {}
 shinyApp(ui =ui, server = server)
