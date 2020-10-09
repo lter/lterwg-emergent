7d314fc4837e9441b5931c8003ac78b3a90f09ae
k8newcomer <44108256+k8newcomer@users.noreply.github.com>
2020-10-09 13:18:30 -0400

    Updated shiny

--- data/app.R
+++ data/app.R
@@ -8,117 +8,55 @@ library(shiny)
 library(dplyr)
 library(readr)
 library(DT)
-
+library(shinyWidgets)
 #Note: The files are loaded onto the local machine. The folder should be on GitHub and it's name is NeonFiles.
 #Make sure to set the working directory as the GitHub "NeonFiles" folder.
 #This can be done by clicking Session --> Set Working Directory --> Choose Directory. Then navigate to this directory.
 
 #Loading in the csv files
-neonFiles <- list.files(pattern = "*.csv")
-dat<-lapply(neonFiles,read.csv)
-
-#adding all of theHARVARD soil files
-#each file has a different number of columns so they will be separated for now
-harvard1 <-  data.frame(dat[1])
-harvard2 <-  data.frame(dat[2])
-harvard3 <-  data.frame(dat[3])
-
-#Need to join the categorial codes and validation files into one big dataframe each
-categoricalcodes <- data.frame(dat[4])
-for(k in 5:23) {
-  categoricalcodes <- rbind(categoricalcodes, data.frame(dat[k]))
-}
-validation <- data.frame(dat[24])
-for(k in 25:44) {
-  validation <- rbind(validation, data.frame(dat[k]))
-}
+soilFieldChem <- read.csv(file = 'soilFieldChem.csv')
 
 
-ui <- fluidPage(
-  title = "NEON Shiny App",
-  sidebarLayout(
+shinyApp(
+  ui = pageWithSidebar(
+    headerPanel("Painting 8"),
     sidebarPanel(
-      selectInput(inputId = "pubCode", "Select pubcode", choices=c("All", sort(unique("pubCode")))),
-      conditionalPanel(
-        'input.dataset === "harvard1"',
-        helpText("Click the tab that you want to look at.")
-      ),
-      conditionalPanel(
-        'input.dataset === "harvard2"',
-        helpText("Click the tab that you want to look at.")
-      ),
-      conditionalPanel(
-        'input.dataset === "harvard3"',
-        helpText("Click the tab that you want to look at.")
-      ),
-      conditionalPanel(
-        'input.dataset === "categoricalCodes"',
-        helpText("Click the tab that you want to look at.")
-      ),
-      conditionalPanel(
-        'input.dataset === "validation"',
-        helpText("Click the tab that you want to look at.")
+      selectizeGroupUI(
+        id = "my-filters",
+        inline = FALSE,
+        params = list(
+          siteID = list(inputId = "siteID", title = "Select variable 1", placeholder = 'select'),
+          nlcdClass = list(inputId = "nlcdClass", title = "Select variable 2", placeholder = 'select'),
+          sampleTiming = list(inputId = "sampleTiming", title = "Select variable 3", placeholder = 'select'),
+          incubationMethod = list(inputId = "incubationMethod", title = "Select variable 4", placeholder = 'select'),
+          domainID = list(inputId = "domainID", title = "Select variable 5", placeholder = 'select')
+        )
       )
     ),
+    
     mainPanel(
-      tabsetPanel(
-        id = 'dataset',
-        tabPanel("harvard1", DT::dataTableOutput("mytable1")),
-        tabPanel("harvard2", DT::dataTableOutput("mytable2")),
-        tabPanel("harvard3", DT::dataTableOutput("mytable3")),
-        tabPanel("categoricalCodes", DT::dataTableOutput("mytable4")),
-        tabPanel("validation", DT::dataTableOutput("mytable5"))
-      )
+      tableOutput("table")
     )
-  )
+  ),
+  
+  server = function(input, output, session) {
+    
+    res_mod <- callModule(
+      module = selectizeGroupServer,
+      id = "my-filters",
+      data = soilFieldChem,
+      vars = c("siteID", "nlcdClass", "sampleTiming", "incubationMethod", "domainID")
+    )
+    
+    output$table <- renderTable({
+      res_mod()
+    })
+    
+  },
+  
+  options = list(height = 500)
 )
 
-server <- function(input, output) {
-  
-  # sorted columns are colored now because CSS are attached to them
-  output$mytable1 <- DT::renderDataTable({
-    reactive_data <- reactive({
-      categoricalcodes %>%
-        filter(variable1 == input$pubCode)    
-      
-    }) 
-    DT::datatable(harvard1, filter='top', options = list(orderClasses = TRUE))
-  })
-  
-  output$mytable2 <- DT::renderDataTable({
-    reactive_data <- reactive({
-      categoricalcodes %>%
-        filter(variable1 == input$pubCode)    
-      
-    }) 
-    DT::datatable(harvard2, filter='top', options = list(orderClasses = TRUE))
-  })
-  
-  output$mytable3 <- DT::renderDataTable({
-    reactive_data <- reactive({
-      categoricalcodes %>%
-        filter(variable1 == input$pubCode)    
-      
-    }) 
-    DT::datatable(harvard3, filter='top', options = list(orderClasses = TRUE))
-  })
-  
-  output$mytable4 <- DT::renderDataTable({
-    reactive_data <- reactive({
-      categoricalcodes %>%
-        filter(variable1 == input$pubCode)    
-      
-    }) 
-    DT::datatable(categoricalcodes, filter='top', options = list(orderClasses = TRUE))
-  })
-  
-  output$mytable5 <- DT::renderDataTable({
-    reactive_data <- reactive({
-      categoricalcodes %>%
-        filter(variable1 == input$pubCode)    
-      
-    }) 
-    DT::datatable(validation, filter='top', options = list(orderClasses = TRUE))
-  })
-}
+
 shinyApp(ui = ui, server = server)
+
