aeccf3b6cda5b1101102a11b0bd38f8e87874950
dblander <dblander@umass.edu>
2020-10-20 12:38:04 -0400

    Created table.R WIP
    This is the R Shiny App for the data tables. I am now going to find the columns to filter through

---
+++ data/table.R
@@ -0,0 +1,76 @@
+#This is the Shiny app for the Neon data showcasing the tables of the data
+#Author: Dvir Blander and Katrina Newcomer
+#First loading in the shiny, dplyr, readr libraries.
+#The shiny library is used because this is a shiny app.
+#The dplyr and readr libraries are used to help read in the data. 
+#The DT library is used for the datatables
+library(plotly)
+library(ggplot2)
+library(shiny)
+library(dplyr)
+library(readr)
+library(DT)
+library(shinyWidgets)
+#Note: The files are loaded onto the local machine. The folder should be on GitHub and it's name is NeonFiles.
+#Make sure to set the working directory as the GitHub "NeonFiles" folder.
+#This can be done by clicking Session --> Set Working Directory --> Choose Directory. Then navigate to this directory.
+
+#Loading in the csv files
+soilFieldChem <- read.csv(file = 'soilFieldChem.csv')
+grass <- soilFieldChem[grep('grassland|Grassland', soilFieldChem$nlcdClass), ]
+forest <- soilFieldChem[grep('forest|Forest', soilFieldChem$nlcdClass), ]
+forestsub <- forest %>% 
+  group_by(siteID, nlcdClass) %>% 
+  summarise(mean_soilMoisture = mean(soilMoisture, na.rm = TRUE),
+            mean_soilTemp = mean(soilTemp, na.rm = TRUE))
+
+grasssub <- grass %>% 
+  group_by(siteID, nlcdClass) %>% 
+  summarise(mean_soilMoisture = mean(soilMoisture, na.rm = TRUE),
+            mean_soilTemp = mean(soilTemp, na.rm = TRUE))
+
+allsub <- soilFieldChem%>% 
+  group_by(siteID, nlcdClass) %>% 
+  summarise(mean_soilMoisture = mean(soilMoisture, na.rm = TRUE),
+            mean_soilTemp = mean(soilTemp, na.rm = TRUE))
+ui <- fluidPage(
+  titlePanel("Neon"),
+  sidebarLayout(position = "left",
+                tabPanel("Table",
+                         sidebarPanel(selectInput("selection1", label = h3("Select nlcdClass"), 
+                                                choices =  c("choose" = "", levels(soilFieldChem$nlcdClass)), selected = 'mixedForest' ),
+                                      selectInput("selection2", label = h3("Select siteID"), 
+                                                choices = c("choose" = "", levels(soilFieldChem$siteID)), selected = 'BART'),
+                                      selectInput("selection4", label = h3("Select biophysicalCriteria"), 
+                                                choices = c("choose" = "", levels(soilFieldChem$biophysicalCriteria)), selected = 'OK - no known exceptions'),
+                                      selectInput("selection5", label = h3("Select sampleTiming"), 
+                                                choices = c("choose" = "", levels(soilFieldChem$sampleTiming)), selected='peakGreenness')
+                         )
+                         
+                         
+                ),
+                mainPanel(DT::dataTableOutput("table"))
+   )
+)
+
+
+server <- function(input, output) {
+  
+  tab <- reactive({ 
+    
+    soilFieldChem %>% 
+      filter(nlcdClass == input$selection1) %>% 
+      filter(siteID == input$selection2) %>%
+      filter(biophysicalCriteria == input$selection4) %>%
+      filter(sampleTiming == input$selection5 )
+    
+  })
+  output$table <-DT::renderDataTable({
+    tab()
+    
+  })
+}
+
+# Create Shiny app objects from either an explicit UI/server pair 
+shinyApp(ui = ui, server = server)
+
